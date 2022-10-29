3bbcb743fd0dc64206066cd802ec5290b977680a
WadeZeak <1263989504@qq.com>
2022-10-29 13:30:41 +0800

    add example 1 2

---
+++ Basic Example/Practice Demo/example-1.py
@@ -0,0 +1,7 @@
+
+# if "__name__ " == "__main__":
+for i in range(1 ,5):
+        for j in range(1 ,5):
+            for k in range(1 ,5):
+                if (i != j ) and (i != j ) and (j !=k ):
+                    print (i ,j ,k)
